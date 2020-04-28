f0a3b0c3fb0abc649cf5c74c9b37b54ccc147c34
sineverba <alessandro.arici@gmail.com>
2020-04-28 20:11:08 +0200

    Add main project file

---
+++ project/__init__.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# instantiate the dependencies
+db = SQLAlchemy()
+cors = CORS()
+
+
+def create_app(script_info=None):
+
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv("APP_SETTINGS")
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+    cors.init_app(app, resources={r"*": {"origins": "*"}})
+
+    from project.api import api
+
+    api.init_app(app)
+
+    # migrate = Migrate(app, db)
+    Migrate(app, db)
+    # Try also add migrate = Migrate() and migrate.init_app(app)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {"app": app, "db": db}
+
+    return app
