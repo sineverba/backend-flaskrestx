586c784b3c0e2866fdd120a403b809ce58c7d707
sineverba <alessandro.arici@gmail.com>
2020-04-29 18:18:57 +0200

    Fix Travis

--- .travis_scripts/travis_deploy.sh
+++ .travis_scripts/travis_deploy.sh
@@ -12,6 +12,6 @@ curl -n -X PATCH https://api.heroku.com/apps/backend-flaskrestx/formation \
   -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
   -H "Authorization: Bearer ${HEROKU_API_KEY}";
 
-curl https://cli-assets.heroku.com/install-ubuntu.sh | sh;
+# curl https://cli-assets.heroku.com/install-ubuntu.sh | sh;
 # heroku run python manage.py db migrate --app $HEROKU_APP_NAME;
 # heroku run python manage.py db upgrade --app $HEROKU_APP_NAME;
