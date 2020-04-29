0ca2b57c7c58603c52978e4dbeedfbf752b846b7
sineverba <alessandro.arici@gmail.com>
2020-04-29 14:14:23 +0200

    Improve code coverage

---
+++ project/tests/unit/test_shell_context.py
@@ -0,0 +1,6 @@
+# Test the shell context
+
+def test_shell_context(test_app):
+    assert len(test_app.shell_context_processors) == 1
+    sc = test_app.make_shell_context()
+    assert sc['app'] == test_app
