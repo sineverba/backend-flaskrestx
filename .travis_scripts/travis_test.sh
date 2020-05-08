8b6d77b72502140523eadc408832e7169c428c19
sineverba <alessandro.arici@gmail.com>
2020-05-08 22:01:39 +0200

    Fix Travis Tests

--- .travis_scripts/travis_test.sh
+++ .travis_scripts/travis_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker exec app python -m pytest "project/tests";
-docker exec app /home/myuser/.local/bin/black project;
-docker exec app /home/myuser/.local/bin/flake8 project;
-docker exec app /home/myuser/.local/bin/isort project/**/*.py;
+docker exec app /home/root/.local/bin/black project;
+docker exec app /home/root/.local/bin/flake8 project;
+docker exec app /home/root/.local/bin/isort project/**/*.py;
